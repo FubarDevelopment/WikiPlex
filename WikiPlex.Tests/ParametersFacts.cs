8e9e19a62b8b367d153079be760728505c831327
matthaw <none@none>
2009-10-01 00:37:06 +0000

    Starting to add parameters extraction for reusability across renderers.
    --HG--
    extra : convert_revision : svn%3A768f3a04-76a6-432f-b7ac-274fd7b03e9c/trunk%4034246

---
+++ WikiPlex.Tests/ParametersFacts.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using WikiPlex.Common;
+using Xunit;
+using Xunit.Extensions;
+
+namespace WikiPlex.Tests
+{
+    public class ParametersFacts
+    {
+        public class ExtractUrl
+        {
+            [Fact]
+            public void Should_throw_ArgumentException_if_url_is_not_found()
+            {
+                var ex = Record.Exception(() => Parameters.ExtractUrl(new[] { "foo=bar" })) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("url", ex.ParamName);
+            }
+
+            [Fact]
+            public void Should_throw_ArgumentException_if_url_is_empty()
+            {
+                var ex = Record.Exception(() => Parameters.ExtractUrl(new[] { "url=" })) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("url", ex.ParamName);
+            }
+
+            [Fact]
+            public void Should_throw_ArgumentException_if_url_is_not_valid()
+            {
+                var ex = Record.Exception(() => Parameters.ExtractUrl(new[] { "url=blah" })) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("url", ex.ParamName);
+            }
+
+            [Fact]
+            public void Should_throw_ArgumentException_if_url_contains_codeplex()
+            {
+                var ex = Record.Exception(() => Parameters.ExtractUrl(new[] { "url=http://www.codeplex.com" })) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("url", ex.ParamName);
+            }
+
+            [Fact]
+            public void Should_return_url_if_valid()
+            {
+                string url = Parameters.ExtractUrl(new[] {"url=http://www.foo.com"});
+
+                Assert.Equal("http://www.foo.com/", url);
+            }
+        }
+
+        public class ExtractAlign
+        {
+            [Fact]
+            public void Should_return_default_if_align_is_not_found()
+            {
+                HorizontalAlign align = Parameters.ExtractAlign(new string[0], HorizontalAlign.Center);
+
+                Assert.Equal(HorizontalAlign.Center, align);
+            }
+
+            [Fact]
+            public void Should_return_default_if_align_is_empty()
+            {
+                HorizontalAlign align = Parameters.ExtractAlign(new[] {"align="}, HorizontalAlign.Center);
+
+                Assert.Equal(HorizontalAlign.Center, align);
+            }
+
+            [Fact]
+            public void Should_throw_ArgumentException_if_align_is_not_defined_on_enum()
+            {
+                var ex = Record.Exception(() => Parameters.ExtractAlign(new[] {"align=foo"}, HorizontalAlign.Center)) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("align", ex.ParamName);
+            }
+
+            [Fact]
+            public void Should_return_align_if_valid()
+            {
+                HorizontalAlign align = Parameters.ExtractAlign(new[] {"align=right"}, HorizontalAlign.Center);
+
+                Assert.Equal(HorizontalAlign.Right, align);
+            }
+
+            [Theory]
+            [InlineData("justify")]
+            [InlineData("notset")]
+            public void Should_throw_ArgumentException_if_align_is_invalid_value(string align)
+            {
+                var ex = Record.Exception(() => Parameters.ExtractAlign(new[] { "align=" + align }, HorizontalAlign.Center)) as ArgumentException;
+
+                Assert.NotNull(ex);
+                Assert.Equal("align", ex.ParamName);
+            }
+        }
+    }
+}
---
+++ WikiPlex/Common/Parameters.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace WikiPlex.Common
+{
+    public static class Parameters
+    {
+        public static string ExtractUrl(ICollection<string> parameters)
+        {
+            string url = GetValue(parameters, "url");
+
+            try
+            {
+                var parsedUrl = new Uri(url, UriKind.Absolute);
+                url = parsedUrl.AbsoluteUri;
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid parameter.", "url");
+            }
+
+            if (url.ToLower().Contains("codeplex.com"))
+                throw new ArgumentException("Invalid parameter.", "url");
+
+            return url;
+        }
+
+        public static HorizontalAlign ExtractAlign(ICollection<string> parameters, HorizontalAlign defaultValue)
+        {
+            string align;
+            if (!TryGetValue(parameters, "align", out align))
+                return defaultValue;
+
+            if (!Utility.IsDefinedOnEnum<HorizontalAlign>(align))
+                throw new ArgumentException("Invalid parameter.", "align");
+
+            var alignment = (HorizontalAlign) Enum.Parse(typeof(HorizontalAlign), align, true);
+            if (alignment != HorizontalAlign.Center && alignment != HorizontalAlign.Left && alignment != HorizontalAlign.Right)
+                throw new ArgumentException("Invalid parameter.", "align");
+
+            return alignment;
+        }
+
+        static string GetValue(IEnumerable<string> parameters, string paramName)
+        {
+            string value;
+            if (!TryGetValue(parameters, paramName, out value))
+                throw new ArgumentException("Missing parameter.", paramName);
+
+            return value;
+        }
+
+        static bool TryGetValue(IEnumerable<string> parameters, string paramName, out string value)
+        {
+            value = null;
+            string paramValue = parameters.FirstOrDefault(s => s.StartsWith(paramName + "=", StringComparison.OrdinalIgnoreCase));
+
+            if (string.IsNullOrEmpty(paramValue))
+                return false;
+
+            paramValue = paramValue.Substring(paramName.Length + 1);
+            if (string.IsNullOrEmpty(paramValue))
+                return false;
+
+            value = paramValue;
+            return true;
+        }
+    }
+}
