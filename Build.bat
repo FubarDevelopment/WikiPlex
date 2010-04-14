b578935d9cfa2b94573433c3ac9392c21fa75a56
matthaw <none@none>
2010-04-14 09:32:45 -0700

    Updating build script and removing documentation from build

--- Build.bat
+++ Build.bat
@@ -1,7 +1,12 @@
 @ECHO off
 
+IF "%1%"=="TEAMCITY" GOTO TEAMCITY
 IF NOT "%1%"=="" GOTO BUILD
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WikiPlex.msbuild /p:Configuration=Debug /t:FullBuild
+GOTO END
+
+:TEAMCITY
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WikiPlex.msbuild /p:Configuration=Release /t:CI
 GOTO END
 
 :BUILD
