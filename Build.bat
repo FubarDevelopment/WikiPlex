404ce628aaf45c45e56c1f8622b9b9e6f7329980
matthaw <none@none>
2010-04-13 17:56:19 -0700

    Upgrading solution / framework version to .NET 4

--- Build.bat
+++ Build.bat
@@ -1,10 +1,10 @@
 @ECHO off
 
 IF NOT "%1%"=="" GOTO BUILD
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe WikiPlex.msbuild /p:Configuration=Debug /t:FullBuild
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WikiPlex.msbuild /p:Configuration=Debug /t:FullBuild
 GOTO END
 
 :BUILD
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe WikiPlex.msbuild /p:Configuration=Debug /t:%1%
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe WikiPlex.msbuild /p:Configuration=Debug /t:%1%
 
 :END
