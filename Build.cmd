078d1bbbeb0576e8f59540362ea63dfaa459d38e
matthaw <none@none>
2012-03-08 14:44:19 -0800

    Testing with new build url

--- Build.cmd
+++ Build.cmd
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 echo abc | powershell -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0\lib\psake\psake.ps1 BuildScript.ps1 -framework '3.5' net35"
 echo abc | powershell -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0\lib\psake\psake.ps1 BuildScript.ps1 -framework '4.0' %*"
